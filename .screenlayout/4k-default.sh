ef9faf11c4bb27fa644d279da59ff6bcf99abc44
EdJeeOnGitHub <edjee96@gmail.com>
2021-12-07 18:26:09 -0600

    added 4k layout

---
+++ .screenlayout/4k-default.sh
@@ -0,0 +1,9 @@
+xrandr --dpi 192 --fb 7680x4320 \
+	--output DisplayPort-0 \
+		--mode 3840x2160 \
+		--pos 0x0 \
+		--rotate normal \
+	--output HDMI-A-0 \
+		--scale 2x2 \
+		--pos 3840x0 \
+		--rotate normal
