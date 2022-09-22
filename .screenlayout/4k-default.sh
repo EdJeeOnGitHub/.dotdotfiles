2e04d0410757c4c5bd7e55e1294069a10eec1db2
EdJeeOnGitHub <edjee96@gmail.com>
2022-09-22 12:02:34 -0500

    new monitors

--- .screenlayout/4k-default.sh
+++ .screenlayout/4k-default.sh
@@ -1,9 +1,2 @@
-xrandr --dpi 192 --fb 7680x4320 \
-	--output DisplayPort-1 \
-		--mode 3840x2160 \
-		--pos 3840x0 \
-		--rotate normal \
-	--output HDMI-A-1 \
-		--scale 2x2 \
-		--pos 0x0 \
-		--rotate normal
+#!/bin/sh
+xrandr --output DisplayPort-0 --mode 3840x2160 --pos 0x0 --rotate normal --output DisplayPort-1 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --output HDMI-A-0 --off --output HDMI-A-1 --off --output DVI-D-0 --off
