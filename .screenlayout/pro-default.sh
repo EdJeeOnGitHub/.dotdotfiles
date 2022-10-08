5a5cd135696e550a611d4e9f3ead655c12f06bab
Ed Jee <edjee96@gmail.com>
2022-10-08 14:48:36 -0500

    screen layout

---
+++ .screenlayout/pro-default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 2880x1800 --pos 0x0 --rotate normal --output DP1 --mode 2880x864 --pos 0x1800 --rotate normal --output DP2 --off --output DP3 --off --output VIRTUAL1 --off
